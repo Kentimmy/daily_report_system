9323bc2bf3fdb73480864af1cb47dd401882f3f9
kent1 <kent1@Blue-K>
2024-03-25 18:26:13 +0900

    daily_report_system完成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -42,32 +42,27 @@ public class EmployeeAction extends ActionBase {
      */
     public void index() throws ServletException, IOException {
 
-        //管理者かどうかのチェック //追記
-        if (checkAdmin()) { //追記
+        if (checkAdmin()) {
 
-            //指定されたページ数の一覧画面に表示するデータを取得
             int page = getPage();
             List<EmployeeView> employees = service.getPerPage(page);
 
-            //全ての従業員データの件数を取得
             long employeeCount = service.countAll();
 
-            putRequestScope(AttributeConst.EMPLOYEES, employees); //取得した従業員データ
-            putRequestScope(AttributeConst.EMP_COUNT, employeeCount); //全ての従業員データの件数
-            putRequestScope(AttributeConst.PAGE, page); //ページ数
-            putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //1ページに表示するレコードの数
+            putRequestScope(AttributeConst.EMPLOYEES, employees);
+            putRequestScope(AttributeConst.EMP_COUNT, employeeCount);
+            putRequestScope(AttributeConst.PAGE, page);
+            putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);
 
-            //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
             String flush = getSessionScope(AttributeConst.FLUSH);
             if (flush != null) {
                 putRequestScope(AttributeConst.FLUSH, flush);
                 removeSessionScope(AttributeConst.FLUSH);
             }
 
-            //一覧画面を表示
             forward(ForwardConst.FW_EMP_INDEX);
 
-        } //追記
+        }
 
     }
 
@@ -78,8 +73,7 @@ public class EmployeeAction extends ActionBase {
      */
     public void entryNew() throws ServletException, IOException {
 
-        //管理者かどうかのチェック //追記
-        if (checkAdmin()) { //追記
+        if (checkAdmin()) {
             putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
             putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //空の従業員インスタンス
 
